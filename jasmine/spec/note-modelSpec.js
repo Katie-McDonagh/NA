5d32b0b369b7225ff4280a3fac53dc4a1d502e85
Katie McDonagh <mcdonagh.katie@outlook.com>
2021-01-12 13:28:42 +0000

    Note stores a text note upon insansiation

---
+++ jasmine/spec/note-modelSpec.js
@@ -0,0 +1,14 @@
+describe("Note", function() {
+  it ("can be instanciated", function(){
+    var note1 = new Note
+
+    expect(note1).toBeInstanceOf(Note);
+  });
+
+  it ("stores a note upon instanciation", function(){
+    var note1 = new Note("I'm a note")
+
+    expect(note1.text).toEqual("I'm a note");
+  });
+
+});
